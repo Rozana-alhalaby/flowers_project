c7d43e09a4946b83caf9f73626bfb6579f89f3f0
Rozana al halabi <150289404+Rozana-alhalaby@users.noreply.github.com>
2025-01-29 12:38:18 +0200

    update

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import { Carousel } from '@fancyapps/ui/dist/carousel/carousel.esm.js';
+import '@fancyapps/ui/dist/carousel/carousel.css';
+
+import './scss/tailwind.css';
+
+const container = document.getElementById("myCarousel");
+const options = { infinite: false };
+
+new Carousel(container, options);
