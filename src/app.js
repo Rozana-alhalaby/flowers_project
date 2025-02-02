df0c9d7e0cf8e6e6a470c9024424cea85a8f5ab3
Rozana al halabi <>
2025-02-02 21:25:47 +0200

    update

--- src/app.js
+++ src/app.js
@@ -2,11 +2,19 @@ import { Carousel } from "@fancyapps/ui/dist/carousel/carousel.esm.js";
 import "@fancyapps/ui/dist/carousel/carousel.css";
 
 import "./css/styles.css";
-
 const container = document.getElementById("myCarousel");
 const options = { infinite: false };
 
 new Carousel(container, options);
 
+window.onload = function(){
+document.getElementById("addRegistration").addEvenetListener("submit",
+  (event) => {
+    event.preventDefault()
+    full_name = document.addRegistration.full_name.value
+    email =  document.addRegistration.email.value
+    message =  document.addRegistration.message.value
 
+   })
+}
 
