0459f46f37efcc9ad1db26d5fdb3fe763f9e24c8
Rozana al halabi <>
2025-02-02 19:57:13 +0200

    update

--- src/app.js
+++ src/app.js
@@ -7,16 +7,6 @@ const container = document.getElementById("myCarousel");
 const options = { infinite: false };
 
 new Carousel(container, options);
-document.querySelectorAll('a').forEach(function(link) {
-    link.addEventListener('click', function(event) {
-        var currentPage = window.location.pathname.replace(/^\/|\/$/g, '');
-        
-        var linkHref = link.getAttribute('href').replace(/^\/|\/$/g, '');
-        if (linkHref === currentPage) {
-            event.preventDefault(); 
-        }
-    });
-});
 
 
 
